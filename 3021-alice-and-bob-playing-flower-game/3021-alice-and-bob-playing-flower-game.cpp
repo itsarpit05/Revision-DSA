7ddb850156e0a53314034a83ca44963e3881ea0c
Arpit Sachan <101167160+itsarpit05@users.noreply.github.com>
2025-08-30 00:34:39 +0530

    Time: 0 ms (100%), Space: 8.6 MB (0%) - LeetHub

---
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    long long flowerGame(int n, int m) {
+           long evenN = n/2;
+        long oddN = n-evenN;
+        long evenM = m/2;
+        long oddM = m - evenM;
+        return evenN * oddM + oddN * evenM;
+    }
+};
